4c5d94029079e3a93076342df41117cb2f8175b7
Dimstuudt <dimassandy2512@gmail.com>
2026-06-26 11:20:58 +0700

    Fix Applayout

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -44,14 +44,14 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
               d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
           </svg>
-          Invoice
+          Client Invoice
         </Link>
 
         <Link :href="route('invoices.all')" :class="navClass(route('invoices.all'))" @click="closeSidebarOnMobile">
           <svg class="w-5 h-5 mr-3 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16"/>
           </svg>
-          Semua Invoice
+          Email Schedule Invoice
         </Link>
 
         <Link :href="route('clients.index')" :class="navClass(route('clients.index'))" @click="closeSidebarOnMobile">

acbe426a76e6011e302814930583bf6ffadd19b9
Dimstuudt <dimassandy2512@gmail.com>
2026-07-07 09:49:17 +0700

    Index Fix

--- public/index.php
+++ public/index.php
@@ -6,15 +6,18 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
+// LOCAL
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
-
-// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
+// PRODUCTION (uncomment ini, comment LOCAL di atas)
+// if (file_exists($maintenance = __DIR__.'/../laravel-app/storage/framework/maintenance.php')) {
+//     require $maintenance;
+// }
+// require __DIR__.'/../laravel-app/vendor/autoload.php';
+// $app = require_once __DIR__.'/../laravel-app/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
